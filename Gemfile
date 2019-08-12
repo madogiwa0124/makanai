04d24813b366855fa969e6a3db331f18d7b3d983
Madogiwa <17684091+Madogiwa0124@users.noreply.github.com>
2019-08-12 17:47:29 +0900

    enable rubocop :cop:

--- Gemfile
+++ Gemfile
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
-source "https://rubygems.org"
+source 'https://rubygems.org'
 
 git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
 
 gem 'rack'
+gem 'rubocop', require: false
--- lib/application.rb
+++ lib/application.rb
@@ -1,10 +1,16 @@
+# frozen_string_literal: true
+
 require 'rack'
+require_relative './router.rb'
 
 module Makanai
   class Application
+    attr_accessor :env
+
     def run!
-      app = Proc.new do |env|
-        ['200', {'Content-Type' => 'text/html'}, ["Hello Rack"]]
+      app = proc do |env|
+        @env = env
+        ['200', { 'Content-Type' => 'text/html' }, ['Hello Rack']]
       end
       Rack::Handler::WEBrick.run app
     end
