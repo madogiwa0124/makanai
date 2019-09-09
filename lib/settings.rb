f1f83a4d83a461f1583d35a6046b88b6da04041b
Madogiwa <17684091+Madogiwa0124@users.noreply.github.com>
2019-09-09 22:33:19 +0900

    :wrench: fix default app settings.

--- lib/settings.rb
+++ lib/settings.rb
@@ -3,8 +3,8 @@
 module Makanai
   class Settings
     APP_ROOT_PATH = Dir.pwd
-    DATABASE_PATH = '/src/db/makanai.db'
-    TEMPLATE_ROOT_PATH = '/src/views/'
-    MIGRATION_ROOT_PATH = '/src/migration/'
+    DATABASE_PATH = '/db/makanai.db'
+    TEMPLATE_ROOT_PATH = '/views/'
+    MIGRATION_ROOT_PATH = '/migration/'
   end
 end
