69f1f1055c3c7d1fd55eee0dba53792853f271ca
Madogiwa <17684091+Madogiwa0124@users.noreply.github.com>
2019-10-01 23:01:27 +0900

    :earth_asia: add Hello World spec.

--- spec/application_spec.rb
+++ spec/application_spec.rb
@@ -7,4 +7,16 @@ describe Makanai::Application do
   it 'respond to call.' do
     expect(Makanai::Application.new(router: nil)).to respond_to :call
   end
+
+  describe '#call' do
+    let(:router) { Makanai::Router.new }
+    let(:app) { Makanai::Application.new(router: router) }
+    before { router.get('/') { 'Hello Makanai!' } }
+
+    it 'Success Hellow World' do
+      response = Rack::MockRequest.new(app).get('/')
+      expect(response.status).to eq 200
+      expect(response.body).to eq 'Hello Makanai!'
+    end
+  end
 end
