f0d6f8addbef6961ea45e017a1cf7de128b29ec5
Madogiwa <17684091+Madogiwa0124@users.noreply.github.com>
2019-10-13 14:49:11 +0900

    :sparkles: add spec for application.

--- spec/application_spec.rb
+++ spec/application_spec.rb
@@ -11,12 +11,33 @@ describe Makanai::Application do
   describe '#call' do
     let(:router) { Makanai::Router.new }
     let(:app) { Makanai::Application.new(router: router) }
-    before { router.get('/') { 'Hello Makanai!' } }
+    before { router.get('/') { 'Hello World!' } }
 
     it 'Success Hellow World' do
       response = Rack::MockRequest.new(app).get('/')
       expect(response.status).to eq 200
-      expect(response.body).to eq 'Hello Makanai!'
+      expect(response.body).to eq 'Hello World!'
+    end
+
+    context 'has query parameter' do
+      let(:response) { Rack::MockRequest.new(app).get('/hello?message=Makanai') }
+      before { router.get('/hello') { |req| "Hello #{req.params['message']}!" } }
+
+      it 'Response Successed' do
+        expect(response.status).to eq 200
+      end
+
+      it 'return message with query parameter' do
+        expect(response.body).to eq 'Hello Makanai!'
+      end
+    end
+
+    context 'routing not found' do
+      let(:response) { Rack::MockRequest.new(app).get('/notfound') }
+
+      it 'return status 404' do
+        expect(response.status).to eq 404
+      end
     end
   end
 end
