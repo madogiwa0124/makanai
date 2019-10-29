75d25feca6cae71e343d9c36c93df5edd3fe03ff
Madogiwa <17684091+Madogiwa0124@users.noreply.github.com>
2019-10-29 23:07:00 +0900

    :floppy_disk: add migration spec.

---
+++ spec/migration_spec.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+require 'makanai/migration'
+require 'makanai/settings'
+
+RSpec.describe Makanai::Migration do
+  let(:root) { Makanai::Settings::APP_ROOT_PATH }
+  let(:migratable_object) { Class.new { include Makanai::Migration }.new }
+
+  describe '#migration_root_path' do
+    it 'return migration root path.' do
+      migration_path = Makanai::Settings::MIGRATION_ROOT_PATH
+      expect(migratable_object.migration_root_path).to eq "#{root}#{migration_path}"
+    end
+  end
+
+  describe '#execute_sql' do
+    let(:path) { "#{root}/spec/db/makanai.db" }
+    let(:create_table_sql_path) { "#{root}/spec/migration/create_numbers.sql" }
+    let(:show_tables_sql) { "select name from sqlite_master where type='table';" }
+
+    before do
+      allow(STDOUT).to receive(:puts)
+      db = Makanai::Database.new(path: path)
+      migratable_object.execute_sql(sql_path: create_table_sql_path, db: db)
+    end
+
+    after do
+      drop_table_sql = File.read("#{root}/spec/migration/drop_numbers.sql")
+      Makanai::Database.new(path: path).execute_sql(drop_table_sql)
+    end
+
+    it 'created numbar table.' do
+      result = Makanai::Database.new(path: path).execute_sql(show_tables_sql)
+      expect(result.map { |table| table['name'] }).to include 'numbers'
+    end
+  end
+end
